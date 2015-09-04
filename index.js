a515b01a950f55c1db84378b7667bf56f7e2cba8
Stepan Kosatyi <stepan@kosatyi.com>
2015-09-04 15:06:50 +0300

    add options definition

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
+/**
+ * GetText Generator
+ * options:
+ * @name = String;
+ * @keywords = Array;
+ * @source   = String;
+ * @target   = String;
+ * @locales  = Array;
+ */
 module.exports = require('./lib/generator');

0d2a9c2e0e2f05c9f83b51741ecda2fd83d43657
Stepan Kosatyi <stepan@kosatyi.com>
2017-10-17 17:30:30 +0300

    update tests

--- tests/test.js
+++ tests/test.js
@@ -1,21 +1,20 @@
 var generator = require('../lib/generator');
 
 
-generator({
-    name: 'messages',
-    keywords:['_'],
-    source:['./source','./templates'],
-    target:'./locale',
-    locales:['ru','en','uk']
-})(function(){
-	
-});
 
-generator.extractTranslation({
-    path    : ['./source','./templates'],
-    match   : /_(?:\(|\s)(?:["'])(.+?)(?:["'])(?:\)|\s?)/g,
-    replace : '_(\'$1\')',
-    target  : './templates.js'
-}).then(function(){
-
+generator.process({
+    extract:{
+        path    : ['./source'],
+        match   : /_(?:\(|\s)(?:["'])(.+?)(?:["'])(?:\)|\s?)/g,
+        replace : '_(\'$1\')',
+        target  : './extract/templates.js'
+    },
+    params : {
+        name: 'messages',
+        language:'Python',
+        keywords:['_'],
+        source:['./source','./extract'],
+        target:'./locale',
+        locales:['ru','en','uk']
+    }
 });
